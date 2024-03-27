758721b4d0216457bc90dae4bdf0ea81d8151a2e
Demarquies Jolley <ahmir1128@gmail.com>
2024-03-27 17:20:32 -0500

    lab 03

--- src/Components/gallery.jsx
+++ src/Components/gallery.jsx
@@ -1,24 +1,25 @@
+/* eslint-disable react/prop-types */
 import './styles.css';
 import {useState} from 'react';
 // eslint-disable-next-line no-unused-vars
-import React from 'react';
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
+import React from 'react';
 import Beasts from './data.json';
 
 // HornedBeast component
 // eslint-disable-next-line react/prop-types
-function HornedBeast ({title, imageUrl, description}) {
+function HornedBeast ({title, imageUrl, description, onBeastClick}) {
   const [favoriteCount, setFavoriteCount] = useState(0);
 
   const increaseFavorites = () => setFavoriteCount(favoriteCount + 1);
   return (
     <>
     <Card style={{ width: '18rem' }}>
-      <Card.Img variant="top" src= {imageUrl} />
+      <Card.Img variant="top" src= {imageUrl} onClick={onBeastClick} />
       <Card.Body>
         <Card.Title>{title}</Card.Title>
         <Card.Text>{description}</Card.Text>
@@ -29,13 +30,14 @@ function HornedBeast ({title, imageUrl, description}) {
 }
 
 // Gallery component
-function Gallery() {
+function Gallery({onBeastClick}) {
   return(
     <Container>
       <Row>
         {Beasts.map((beast, index) =>(
           <Col key={index} md={4}>
             <HornedBeast
+            onBeastClick={() => onBeastClick(beast)}
             title={beast.title}
             imageUrl={beast.image_url}
             description= {beast.description}
